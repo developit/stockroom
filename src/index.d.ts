3f33ca8b431a46b971a5f139d922e42c7f21ee34
Jordan Mele <SiliconSoldier@outlook.com.au>
2019-01-14 19:23:38 +1100

    Removed useless TS code comments and documented generics

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,5 @@
+// K - Store state
+
 import { Store } from "unistore";
 
-/**
- * Given a Web Worker instance, sets up RPC-based synchronization with a WorkerStore running within it.
- * @param worker An instantiated Web Worker (eg: `new Worker('./store.worker.js')`)
- * @returns A mock unistore store instance sitting in front of the worker store.
- * @example
- *	import createStore from 'stockroom'
-	*	import StoreWorker from 'worker-loader!./store.worker'
-	*	let store = createStore(new StoreWorker)
-	*/
 export default function createStore<K>(worker: Worker): Store<K>;
--- src/worker.d.ts
+++ src/worker.d.ts
@@ -1,28 +1,10 @@
+// K - Store state
+
 import { Store } from "unistore";
 
 export interface WorkerStore<K> extends Store<K> {
-	/**
-	 * Queue all additional processing until unfrozen.
-	 */
 	freeze: () => void;
-	/**
-	 * Remove a freeze lock and process queued work.
-	 */
 	unfreeze: () => void;
 }
 
-/**
- * Creates a unistore instance for use in a Web Worker that synchronizes itself to the main thread.
- * @param initialState Initial state to populate
- * @returns Enhanced unistore store
- * @example
- *	import createWorkerStore from 'stockroom/worker'
- *	let initialState = { count: 0 }
- *	let store = createWorkerStore(initialState)
- *	store.registerActions({
- *		increment(state) {
- *			return { count: state.count + 1 }
- *		}
- *	})
- */
 export default function createWorkerStore<K>(initialState?: K): WorkerStore<K>;
